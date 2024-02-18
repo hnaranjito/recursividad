081770c40d187535151406c0978a9f2cf10a0704
hnaranjo <hnaranjog@gmail.com>
2024-02-18 12:44:35 -0500

    Update endpoints

--- main.py
+++ main.py
@@ -1,56 +1,14 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, Body, Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse
+from data import movies
+
+from model.Movie import Movie
+from model.Category import Category
+
 
 app = FastAPI()
 app.title = "Mi aplicación con FastApi"
 app.version = "0.0.1"
-
-
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str
-    overview: str
-    year: int
-    rating: float
-    category: str
-
-
-movies = [
-    {
-        "id": 1,
-        "title": "Avatar",
-        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi,",
-        "year": 2009,
-        "rating": 7.8,
-        "category": "Acción"
-    },
-    {
-        "id": 2,
-        "title": "Kingsman: El círculo de oro",
-        "overview": "Cuando el cuartel general de la agencia británica Kingsman es destruido, los espías unen sus fuerzas con una organización aliada estadounidense. Su objetivo es colaborar para intentar derrotar a su enemigo común y salvar al mundo",
-        "year": 2017,
-        "rating": 7.7,
-        "category": "Acción"
-    },
-    {
-        "id": 3,
-        "title": "Resistencia",
-        "overview": "Un antiguo agente de las fuerzas especiales se encuentra al mando de un comando que debe cazar y matar al creador de una inteligencia artificial extraordinaria que tiene la capacidad de aniquilar a toda la humanidad",
-        "year": 2023,
-        "rating": 7.4,
-        "category": "Acción"
-    },
-    {
-        "id": 4,
-        "title": "Resistencia",
-        "overview": "Un antiguo agente de las fuerzas especiales se encuentra al mando de un comando que debe cazar y matar al creador de una inteligencia artificial extraordinaria que tiene la capacidad de aniquilar a toda la humanidad",
-        "year": 2023,
-        "rating": 7.4,
-        "category": "Acción"
-    }
-]
 
 
 @app.get('/', tags=['Home'])
@@ -60,26 +18,27 @@ def message():
 
 @app.get('/movies', tags=['movies'])
 def get_movies():
-    return movies
+    return JSONResponse(content=movies)
 
 
 @app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int):
+def get_movie(id: int = Path(ge=1, le=2000)):
     for item in movies:
         if item["id"] == id:
-            return item
-    return []
+            return JSONResponse(content=item)
+    return JSONResponse(content=[])
 
 
 @app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category: str, year: int):
-    return [item for item in movies if item["category"] == category]
+def get_movies_by_category(category: str = Query(min_length=5)):
+    data = [item for item in movies if item["category"] == category]
+    return JSONResponse(content=data)
 
 
 @app.post('/movies', tags=['movies'])
 def create_movie(movie: Movie):
     movies.append(movie)
-    return movies
+    return JSONResponse(content={"message": "Se ha registrado la película"})
 
 
 @app.put('/movies/{id}', tags=['movies'])
@@ -91,7 +50,7 @@ def update_movie(id: int, movie: Movie):
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-            return movies
+            return JSONResponse(content={"message": "Se ha modificado la película"})
 
 
 @app.delete('movies/{id}', tags=['movies'])
@@ -99,4 +58,4 @@ def delete_movie(id: int):
     for item in movies:
         if item["id"] == id:
             movies.remove(item)
-            return movies
+            return JSONResponse(content={"message": "Se ha eliminado la película"})
